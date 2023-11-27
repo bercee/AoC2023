51a5f6206d085625f4d4ebc8f907c77bbd9bd2d6
Bertalan Kovacs-Garai <bkovacs@chemaxon.com>
2023-11-27 11:53:20 +0100

    day 2 solution

--- src/impl/2021/day2.ts
+++ src/impl/2021/day2.ts
@@ -1,21 +1,60 @@
 import { Solver } from "../solver";
-import { asIntArray } from "../../util/inputParser";
+import { Parsers } from "../../util/inputParser";
+import { Vector } from "ts-matrix";
+
+
 
 export default class Day2 extends Solver {
 
-    private readonly array: number[];
+    input: string[][];
 
-    constructor(input: string) {
-        super(input);
-        this.array = asIntArray(input);
+    private steps: Array<Vector> = [];
+
+    constructor(props: string) {
+        super(props);
+        this.parseSteps();
     }
+
 
     part1(): string | number {
-        return "";
+        // console.log(this.steps);
+        let p = new Vector([0,0]);
+        this.steps.forEach(s => p = p.add(s));
+        // console.log(p)
+        return  p.at(0) * p.at(1);
     }
+
 
     part2(): string | number {
-        return "";
+        let h = 0;
+        let d = 0;
+        let a = 0;
+        this.input.forEach(l => {
+            if (l[0].startsWith("f")) {
+                h+=Number.parseInt(l[1]);
+                d+=(a*Number.parseInt(l[1]))
+            } else if (l[0].startsWith("d")) {
+                a+=Number.parseInt(l[1]);
+            } else {
+                a-=Number.parseInt(l[1]);
+            }
+        })
+        return h*d;
     }
 
+    private parseSteps() {
+        this.input.forEach(l => {
+            if (l[0].startsWith("f")) {
+                this.steps.push(new Vector([Number.parseInt(l[1]), 0]));
+            } else if (l[0].startsWith("d")) {
+                this.steps.push(new Vector([0, Number.parseInt(l[1])]));
+            } else {
+                this.steps.push(new Vector([0, -Number.parseInt(l[1])]));
+            }
+        })
+    }
+
+    protected parse(input: string) {
+        return Parsers.asMatrix(input);
+    }
 }
--- src/util/inputParser.ts
+++ src/util/inputParser.ts
@@ -2,10 +2,10 @@
 export type InputParser = (input: string) => any;
 
 export class Parsers {
-    static readonly asArray = (input: string) => input.split("\n");
+    static readonly asArray = (input: string) => input.split("\n").filter(l => l.trim().length);
 
-    static readonly asIntArray = (input: string) => input.split("\n").map(s => Number.parseInt(s));
+    static readonly asIntArray = (input: string) => Parsers.asArray(input).map(s => Number.parseInt(s));
 
-    static readonly asMatrix = (input: string) => Parsers.asArray(input).map(s => s.split(/s+/));
+    static readonly asMatrix = (input: string) => Parsers.asArray(input).map(s => s.split(/\s+/));
 
 }
