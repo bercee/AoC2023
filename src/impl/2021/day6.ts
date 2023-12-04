553276e127de189b7cbde1fead0ab525aaaf9346
Bertalan Kovacs-Garai <bertalan1990@gmail.com>
2023-12-04 12:16:56 +0100

    solve test tasks for 2021

--- src/impl/2021/day6.ts
+++ src/impl/2021/day6.ts
@@ -1,43 +1,39 @@
 import { Solver } from "../solver";
+import _ from "lodash";
 
-interface Fish {
-    d: number;
-}
+
 export default class Day6 extends Solver {
 
-    readonly list: Fish[];
+
+    fishes: number[] = Array(9).fill(0);
 
     constructor(origInput: string) {
         super(origInput);
-        this.list = origInput.split(",").map(s => Number.parseInt(s)).map(n => {return {d: n}});
+        this.origInput.split(",").forEach(s => this.fishes[Number.parseInt(s)]++)
+        // console.log(`init: ${this.fishes.join(",")}`);
     }
 
     part1(): string | number {
-        console.log(this.list.map(f => f.d));
-        for (let i = 0; i < 80; i++) {
-            this.iterate();
-        }
-        return this.list.length;
+        this.iterate(80);
+        return _.sum(this.fishes);
     }
 
-    private iterate() {
-        let newFish: Fish[] = [];
-        this.list.forEach(f => {
-            f.d--;
-            if (f.d === -1) {
-                f.d = 6;
-                newFish.push({d: 8});
-            }
-        })
-        newFish.forEach(f => this.list.push(f));
-    }
 
     part2(): string | number {
-        for (let i = 0; i < 256; i++) {
-            console.log(`day ${i} fishes ${this.list.length}`)
-            this.iterate();
+        this.iterate(256);
+        return _.sum(this.fishes);
+    }
+
+    private iterate(days: number) {
+        for (let i = 0; i < days; i++) {
+            const zeros = this.fishes[0];
+            for (let f = 0; f < 8; f++) {
+                this.fishes[f] = this.fishes[f+1];
+            }
+            this.fishes[8] = zeros;
+            this.fishes[6] += zeros;
+            // console.log(`day: ${i}, fishes: ${this.fishes.join(",")}`)
         }
-        return this.list.length;
     }
 
 }
--- test.ts
+++
@@ -1,8 +0,0 @@
-import { getInput, getTestInput, getTestResult } from "./src/api/contentLoader.js";
-
-console.log("helo")
-
-
-getInput(2021, 22).then(console.log)
-getTestInput(2021, 1).then(console.log)
-getTestResult(2022,5,1).then(console.log)
